0742a4b185566dd3ad8aaa3d1edfb3bf762b7466
Giovanni Cimmino <cimminogi@gmail.com>
2016-10-07 17:51:41 +0200

    first commit

---
+++ lib/rails_imei_validator.rb
@@ -0,0 +1,18 @@
+require "rails_imei_validator/version"
+require 'active_model'
+
+class RailsImeiValidator
+  def valid(imei)
+      return false if imei.blank?
+      digits = imei.reverse.chars.map(&:to_i)
+      digits.each_with_index.inject(0) do |sum, (digit, i)|
+        digit *= 2 if i.odd?
+        digit -= 9 if digit > 9
+        sum += digit
+      end
+  end
+
+  def validate_each(record, attribute, value)
+    record.errors.add(attribute, :invalid) unless valid(imei)
+  end
+end
---
+++ lib/rails_imei_validator/version.rb
@@ -0,0 +1,3 @@
+module RailsImeiValidator
+  VERSION = "0.1.0"
+end
